9d013c9a1876507e847cedc6f29da327e0cd14e2
aodong.qin <ioperations.c@gmail.com>
2021-09-21 17:01:26 +0000

    update target rm

--- hello_macro/target/debug/deps/hello_macro-39e6717500f3059d.d
+++
@@ -1,5 +0,0 @@
-/home/tablinux/Github/hello_macro_rs/hello_macro/target/debug/deps/hello_macro-39e6717500f3059d.rmeta: src/lib.rs
-
-/home/tablinux/Github/hello_macro_rs/hello_macro/target/debug/deps/hello_macro-39e6717500f3059d.d: src/lib.rs
-
-src/lib.rs:
--- hello_macro/target/debug/deps/hello_macro-43f0b9d1f98f25da.d
+++
@@ -1,5 +0,0 @@
-/home/tablinux/Github/hello_macro_rs/hello_macro/target/debug/deps/hello_macro-43f0b9d1f98f25da.rmeta: src/lib.rs
-
-/home/tablinux/Github/hello_macro_rs/hello_macro/target/debug/deps/hello_macro-43f0b9d1f98f25da.d: src/lib.rs
-
-src/lib.rs:
