562879d52bc0ee97dade1f7e0d81c5d308486f61
aodong.qin <ioperations.c@gmail.com>
2021-09-21 16:37:59 +0000

    a bunch of process

--- hello_macro_derve/src/lib.rs
+++ hello_macro_derve/src/lib.rs
@@ -1,16 +1,43 @@
 extern crate proc_macro;
+
 use proc_macro::TokenStream;
-use syn;
 use quote::quote;
+use syn::{self, Fields};
 
 #[proc_macro_derive(HelloMacro)]
-pub fn hello_macro_derive(input: TokenStream) -> TokenStream{
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse(input).unwrap();
     impl_hello_macro(&ast)
 }
 
 fn impl_hello_macro(ast: &syn::DeriveInput) -> TokenStream {
     let name = &ast.ident;
+    println!("the whole struct is {:#?}", ast.ident);
+    // println!("data is {:#?}",ast.data);
+    let _ = match &ast.data {
+        syn::Data::Struct(data_struct) => match &data_struct.fields {
+            Fields::Named(name) => {
+                let mut counter = 0;
+                for i in name.named.iter() {
+                    println!(" ==> the {}'s filed ident is {:#?}",counter , i.ident,);
+                    
+                    for j in i.attrs.iter(){
+                        println!("{} has attribute {:#?}" ,std::iter::repeat("-").take(10).collect::<String>(),
+                        j.tokens);
+                    }
+                    counter+=1;
+                }
+            }
+
+            Fields::Unnamed(_) => todo!(),
+            Fields::Unit => todo!(),
+        },
+        syn::Data::Enum(data_enum) => {
+            println!("{:#?}", data_enum.variants.last().unwrap().ident);
+        }
+        syn::Data::Union(_) => {}
+    };
+
     let gen = quote! {
         impl HelloMacro for #name {
             fn hello_macro() {
--- hello_macro_test/src/main.rs
+++ hello_macro_test/src/main.rs
@@ -1,11 +1,33 @@
 use hello_macro::HelloMacro;
 use hello_macro_derve::HelloMacro;
+use serde::{Serialize,Deserialize};
+
+#[derive(Debug,Clone, Copy,PartialEq,PartialOrd)]
+#[derive(Serialize,Deserialize)]
+enum Color {
+    #[allow(unused)]
+    Red,
+    #[allow(unused)]
+    Blue,
+    #[allow(unused)]
+    Green,
+}
 
 #[derive(HelloMacro)]
-struct Packents;
+#[derive(Clone,Debug, PartialEq, PartialOrd)]
+#[derive(Serialize,Deserialize)]
+struct Packents {
+    #[allow(unused)]
+    #[serde(skip_serializing)]
+    name: u32,
+    #[allow(unused)]
+    #[serde(skip_serializing)]
+    good: &'static str,
+    #[allow(unused)]
+    #[serde(skip_serializing)]
+    test: Color,
+}
 
 pub fn main() {
     Packents::hello_macro();
 }
-
-
